08efca4c916ee44512f6701f58f69fa6883c0c5a
Latoyya Smith <latoyyas@gmail.com>
2017-08-02 11:48:51 -0500

    uploaded HTTP-Mini-Sprint

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,9 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const promise = axios.get('http://localhost:5000/friends');
+    return {
+        type: GET_FRIENDS,
+        payload: promise,
+    };
 };
